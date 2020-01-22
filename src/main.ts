23a45f832bb82781d526606bc34fe2d6a166d6e8
MaryAlvarezH <maria.alvarez9409@gmail.com>
2020-01-22 15:13:58 -0600

    add web-components dependence

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,26 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode } from "@angular/core";
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import { AppModule } from "./app/app.module";
+import { environment } from "./environments/environment";
+
+// import { applyPolyfills, defineCustomElements } from "../loader";
+
+import {
+  applyPolyfills,
+  defineCustomElements
+} from "../node_modules/epy-components/loader";
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+applyPolyfills().then(() => {
+  defineCustomElements(window);
+});
+
+// defineCustomElements(window);
